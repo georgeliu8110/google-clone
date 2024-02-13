2b923e4d9178d5c9c11dae5ded6813ff04b2a0e5
George Liu <georgeliu8110@gmail.com>
2024-02-13 17:32:29 -0600

    add footer

---
+++ src/app/components/CountryLookup.jsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+
+export default function CountryLookup() {
+
+  const [country, setCountry] = useState('')
+
+  useEffect(() => {
+    const getcountry = async () => {
+      const res = await fetch(`https://extreme-ip-lookup.com/json/?key=${process.env.NEXT_PUBLIC_IP_API_KEY}`)
+      const data = await res.json()
+      console.log(data)
+      setCountry(`${data.city}, ${data.region}, ${data.country}`)
+    }
+    getcountry()
+    console.log(country)
+  }, [])
+
+  return (
+    <div>
+      <h1>{country}</h1>
+    </div>
+  );
+}
