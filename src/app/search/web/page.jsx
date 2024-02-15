d3b6da1808d1f907cde139fc476fe550850f9da9
George Liu <georgeliu8110@gmail.com>
2024-02-15 10:20:11 -0600

    add error handling

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,7 +1,31 @@
-export default function WebSearchPage() {
+import Link from 'next/link'
+
+export default async function WebSearchPage({searchParams}) {
+  const term = await searchParams.searchTerm
+
+  const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CX_KEY}&q=${term}`);
+
+  const data = await response.json()
+  const results = await data.items
+
+  if (!results) {
+    return (
+      <div className='justify-center flex flex-col items-center bg-white pt-10'>
+        <h1 className='text-3xl pb-2'>No results found for {term}</h1>
+        <p className='text-lg pb-2'>Please try searching for something else </p>
+        <Link className=' text-blue-500' href={'/'}> Home
+        </Link>
+      </div>
+    )
+  }
+
   return (
     <div>
-      WebSearchPage
+      {results.map(result=>{
+        return (
+          <p key={result.cacheId}>{result.title}</p>
+        )
+      })}
     </div>
   )
 }
