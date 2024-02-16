fbb38bbf1373012660682bf80e2e18bdcce85ea9
George Liu <georgeliu8110@gmail.com>
2024-02-16 00:41:59 -0600

    fix suspense boundary error

--- src/app/components/SearchHeader.jsx
+++ src/app/components/SearchHeader.jsx
@@ -4,6 +4,7 @@ import SearchBox from './SearchBox'
 import { RiSettings3Line } from "react-icons/ri"
 import { PiDotsNine } from "react-icons/pi";
 import SearchHeaderOptions from './SearchHeaderOptions'
+import { Suspense } from 'react';
 
 export default function SearchHeader() {
 
@@ -14,7 +15,9 @@ export default function SearchHeader() {
           <Image src='/google_logo.jpeg' alt="google logo" width={100} height={50} className=''/>
         </Link>
         <div className='flex-1'>
+        <Suspense fallback={<div>Loading...</div>}>
           <SearchBox ></SearchBox>
+        </Suspense>
         </div>
         <div className='hidden md:inline-flex space-x-2'>
           <RiSettings3Line className='bg-transparent hover:bg-gray-200 p-2 text-4xl rounded-full cursor-pointer'></RiSettings3Line>
@@ -22,7 +25,9 @@ export default function SearchHeader() {
         </div>
         <button className='bg-blue-500 text-white rounded-md px-6 py-2 font-medium hover:brightness-105 hover:shadow-md transition-shadow ml-2'>Sign In</button>
       </div>
+      <Suspense fallback={<div>Loading...</div>}>
       <SearchHeaderOptions></SearchHeaderOptions>
+      </Suspense>
     </header>
   )
 }
